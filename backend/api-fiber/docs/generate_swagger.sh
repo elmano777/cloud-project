c091fdc22920ac44b7d9142d7e999a78615ce283
elmano777 <rolandodiazjara@gmail.com>
2025-05-10 01:53:47 -0500

    .

--- backend/api-fiber/docs/generate_swagger.sh
+++ backend/api-fiber/docs/generate_swagger.sh
@@ -5,7 +5,6 @@
 
 # Generar la documentación de Swagger
 echo "Generando documentación de Swagger..."
-swag init -g app/boostrap/app.go -o ./docs
-
+swag init -g app/bootstrap/app.go -o ./docs --parseDependency --parseInternal
 echo "Documentación generada en la carpeta docs/"
 echo "Puedes acceder a la interfaz de Swagger en: http://localhost:8070/swagger"
