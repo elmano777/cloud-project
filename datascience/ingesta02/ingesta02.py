53e6ab10a037ed6b20781300787e90a36aeef381
IanCortez <iancortezg@hotmail.com>
2025-05-11 17:18:31 -0500

    fix: Actualizado el proceso de descarga y subida de archivos

--- datascience/ingesta02/ingesta02.py
+++ datascience/ingesta02/ingesta02.py
@@ -1,23 +1,26 @@
+# PostgreSQL tables
 import psycopg2
 import boto3
+import csv
 
 
 HOST = "172.31.86.125" # IPv4 privada de "MV Bases de Datos"
 PORT = "5342"
-USERNAME = "root"
-PASSWORD = "utec"
-DATABASE_NAME = "bd_api_employees"  
+USERNAME = "postgres"
+PASSWORD = "postgres"
+DATABASE_NAME = "usuario"  
 
 
 # Cambiar el nombre del bucket
 BUCKET = "ingesta-bd-storage"
 UPLOAD_FILES = [
-    ("usuarioData.csv", "usuario"),
-    ("ejerceData.csv", "ejerce")
+    ("usuariosData.csv", "usuarios"),
+    ("ejerce.csv", "ejerce")
 ]
 
 
-def get_user_data(table_name: str):
+
+for filename, table in UPLOAD_FILES:
     conn = psycopg2.connect(
         host=HOST,
         port=PORT,
@@ -25,21 +28,22 @@ def get_user_data(table_name: str):
         password=PASSWORD,
         database=DATABASE_NAME
     )  
+
     cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM {table_name}")
+    cursor.execute(f"SELECT * FROM {table}")
+    columnas = [desc[0] for desc in cursor.description]
     result = cursor.fetchall()
+
+    with open(filename, mode="w", newline='', encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(columnas)
+        writer.writerow(result)
+
     cursor.close()
     conn.commit()
     conn.close()
-    return result
-
-
-for filename, table in UPLOAD_FILES:
-    with open(filename, "w") as file:
-        res = get_user_data(table)
-
 
     s3 = boto3.client('s3')
     response = s3.upload_file(filename, BUCKET, filename)
 
-    print(f"Ingesta de {table} completada")
+    print(f"Ingesta de {table} en {filename} completada")
